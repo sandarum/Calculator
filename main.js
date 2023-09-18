89c2ff6f45ef1c4e538748045d7de8c4fea98092
Sandaruwan <sandar.m7373@gmail.com>
2023-09-18 14:55:39 +0300

    modify functions

--- main.js
+++ main.js
@@ -1,37 +1,148 @@
-const output = document.querySelector('.output');
+const currentOperation = document.querySelector('.currentOperation');
+const previousOperation = document.querySelector('.previousOperation');
 
-function setDisplayValue(value) {
-    output.textContent = value;
+let currentNumber = '';
+let previousNumber = '';
+let selectedOperator = undefined; 
+
+function setCurrentDisplay(value) {
+    currentOperation.textContent = value;
 }
 
-function getDisplayValue() {
-    return output.textContent;
+function setPreviousDisplay(value) {
+    if( selectedOperator != null){
+        console.log(selectedOperator);
+        previousOperation.textContent = `${value} ${selectedOperator}`;
+    }else{
+        previousOperation.textContent = value;
+    }
+}
+
+function updateDisplay(current, previous) {
+    setCurrentDisplay(current);
+    setPreviousDisplay(previous);
+}
+
+function getCurrentDisplay() {
+    return currentOperation.textContent;
 }
 
 function appendNumber(value) {
-    if (getDisplayValue() === '0') {
-        setDisplayValue(value);
+    if (getCurrentDisplay() === '0') {
+        setCurrentDisplay(value);
     } else {
-        setDisplayValue(getDisplayValue().concat(value));
+        setCurrentDisplay(getCurrentDisplay().concat(value));
     }
 }
 
 function appendDot() {
-    if(!getDisplayValue().includes('.')){
-        setDisplayValue(getDisplayValue().concat('.'));
+    if(!getCurrentDisplay().includes('.')){
+        setCurrentDisplay(getCurrentDisplay().concat('.'));
     }
 }
 
 function clear() {
-    setDisplayValue('0');
+    currentNumber = '';
+    previousNumber = '';
+    selectedOperator = undefined;
+    updateDisplay('0','');
 }
 
 function deleteValue() {
-    if(getDisplayValue().length !== 1) {
-        setDisplayValue(getDisplayValue().slice(0,-1));
+    if(getCurrentDisplay().length !== 1) {
+        setCurrentDisplay(getCurrentDisplay().slice(0,-1));
     }else{
-        setDisplayValue('0');
+        setCurrentDisplay('0');
     }
 }
 
-setDisplayValue('0');
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return b === 0 ? 'Error' : a/b;
+}
+
+function compute(){
+    let result;
+    let current = Number(currentNumber);
+    console.log(`Current Value: ${current}`);
+    let prev = Number(previousNumber);
+    console.log(`Previous Value: ${prev}`);
+    if( Number.isNaN(currentNumber) || Number.isNaN(previousNumber) ){
+        return;
+    }
+    switch(selectedOperator) {
+        case '+':
+            result = add(prev, current);
+            break;
+        case '−':
+            result = substract(prev, current);
+            break;
+        case '×':
+            result = multiply(prev, current);
+            break;
+        case '÷':
+            result = divide(prev, current);
+            break;
+        default:
+            return;
+    }
+    currentNumber = result;
+    console.log(`Result: ${result}`);
+    previousNumber = '';
+    selectedOperator = undefined;
+    updateDisplay(currentNumber, previousNumber);
+}
+
+function setOperator(operator) {
+    currentNumber = getCurrentDisplay();
+    if(currentNumber === '') {
+        selectedOperator = operator;
+        return;
+    }
+    if(previousNumber !== ''){
+        compute();
+    }
+    selectedOperator = operator;
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+
+setCurrentDisplay('0');
+
+buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.addEventListener('click', () => {
+    if(!Number.isNaN(Number(button.textContent))){
+        appendNumber(button.textContent);
+    }else {
+        switch(button.textContent){
+            case 'Clear':
+                clear();
+                break;
+            case 'Del':
+                deleteValue();
+                break;
+            case '.':
+                appendDot();
+                break;
+            case '=':
+                setOperator(undefined);
+                updateDisplay(currentNumber, previousNumber);
+                break;
+            default:
+                setOperator(button.textContent);
+                updateDisplay(currentNumber, previousNumber);
+        }
+    }
+}));
+
